85e07dcb2bfb0b83925ac30d050686b62eefb453
lrbarros <luizricardobarros@gmail.com>
2021-08-12 22:36:08 -0300

    state e alteração de variavel

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -4,51 +4,84 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Stack(
-        children: [
-          Image.asset(
-            "images/restaurant.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Pessoas:0",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            '+1',
-                            style: TextStyle(fontSize: 40, color: Colors.white),
-                          ))),
-                  Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            '-1',
-                            style: TextStyle(fontSize: 40, color: Colors.white),
-                          )))
-                ],
-              ),
-              Text(
-                "Pode entrar",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                    fontStyle: FontStyle.italic),
-              )
-            ],
-          ),
-        ],
-      )));
+      home: Home(key:null),
+    )
+      );
 }
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText ="";
+
+  void _changePeople(int delta){
+    setState(() {
+      _people += delta;
+
+      if(_people <= 10){
+        _infoText = "Pode Entrar!";
+      }else{
+        _infoText ="Lotado!";
+      }
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pessoas:$_people",
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextButton(
+                        onPressed: () {_changePeople(1);},
+                        child: Text(
+                          '+1',
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        ))),
+                Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextButton(
+                        onPressed: () {_changePeople(-1);},
+                        child: Text(
+                          '-1',
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        )))
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontStyle: FontStyle.italic),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
